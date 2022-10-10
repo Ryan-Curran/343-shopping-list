bfb580f164c6fc961ebba42912eb22ecedb91799
Ryan Curran <91218376+Ryan-Curran@users.noreply.github.com>
2022-10-10 13:25:45 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,18 @@
 /* define your functions here */
 
-
-
-
-
-
-
-
-        
+function outputCartRow(item, total) {
+  const {
+    product: { title, filename, price},
+    quantity,
+  } = item;
+  console.log(title, filename, price, quantity);
+  const row = `       <tr>
+            <td><img src="images/${filename}
+            "></td>
+            <td>${title}</td>
+            <td>${quantity}</td>
+            <td>${price}</td>
+            <td>${total}</td>
+        </tr>`;
+  document.write(row);
+}
